dba37d6eb4977d64e5b1b194db887b35b8892195
viccabezos <cabezosvic@gmail.com>
2021-06-21 16:38:34 +0200

    get issues from api

--- src/components/github.jsx
+++ src/components/github.jsx
@@ -1,5 +1,26 @@
-import react from "react";
+import React, { useEffect, useState } from "react";
+
+import { Octokit } from "@octokit/rest";
 
 export default function Github() {
+  const { Octokit } = require("@octokit/rest");
+  const [issues, SetIssues] = useState([]);
+  const octokit = new Octokit({
+    auth: " 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",
+  });
+
+  useEffect(() => {
+    async function getIssues() {
+      const { data } = await octokit.request(
+        "GET /repos/facebook/react/issues",
+        { owner: "facebook", repo: "react" }
+      );
+
+      SetIssues(data);
+    }
+
+    getIssues();
+  }, []);
+
   return <div>github issues list</div>;
 }

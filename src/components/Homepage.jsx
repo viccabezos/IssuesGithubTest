8dda79bf674bf6ba5c4c7f4741d67b311d312e76
viccabezos <cabezosvic@gmail.com>
2021-06-22 20:28:22 +0200

    welcome with logo

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import gitlogo from "../images/gitlogo.svg"
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>Welcome</h2>
+      <div className="flex flex-col justify-center">
+        <img src={gitlogo} alt="github" className="flex m-auto w-3/5" />
+        <h1 className="text-center text-xl  m-14">Welcome</h1>
       </div>
     );
   }
